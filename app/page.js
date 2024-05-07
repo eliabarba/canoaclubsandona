1e40a05fff210335a7b1316fce4db7291654af73
eliabarba <115450000+eliabarba@users.noreply.github.com>
2024-05-07 10:47:39 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styled from "./page.module.css";
 import Present from "@/components/Present";
 import Carousel from "@/components/Carousel";
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styled.main}>
       <Carousel />
 
     </main>
