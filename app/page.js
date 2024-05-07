467853895392b909fee29d325ccf51b540e075fc
eliabarba <115450000+eliabarba@users.noreply.github.com>
2024-05-07 10:31:19 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Present from "@/components/Present";
-import SwipeableTextMobileStepper from "@/components/Carousel";
+import Carousel from "@/components/Carousel";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <SwipeableTextMobileStepper />
+      <Carousel />
 
     </main>
   );
